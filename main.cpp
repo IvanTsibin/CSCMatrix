e32f70eefbfe1a630524c03fbaac6c7f3a2b6582
Ivan Tsibin <ivan_tsibin@list.ru>
2024-07-16 16:29:52 +0700

    corrcted CSC Matrix saving - check is required

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	CSCMatrix new_matrix(4, 20);
+	CSCMatrix new_matrix(5, 20);
 	new_matrix.saveCSCMatrixAsCSCMatrix("csc_matrix.csv");
 	new_matrix.saveCSCMatrixAsStandartMatrix("StandartMatrix.csv");
 	return 0;
